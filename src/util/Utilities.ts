739bce5ebb25c70de125725a425c10dc6cb11e21
Car <cararr@tlen.pl>
2021-08-31 19:39:04 +0200

    Specified locale for dateFormater.

--- src/util/Utilities.ts
+++ src/util/Utilities.ts
@@ -2,7 +2,7 @@ import { Employee, Comment, StationSchedules } from '../types';
 
 export default class Utilities {
 	static formatDateString(date: Date) {
-		const dateArray = date.toLocaleDateString().split('.');
+		const dateArray = date.toLocaleDateString('pl-PL').split('.');
 		const day = Number(dateArray[0]) < 10 ? `0${dateArray[0]}` : dateArray[0];
 		return `${dateArray[2]}-${dateArray[1]}-${day}`;
 	}
